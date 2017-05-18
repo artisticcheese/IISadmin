609161afe6766e00e02ef2509a2f45d95cc5721b
Suvalian, Greg <Greg_Suvalian@McAfee.com>
2017-05-18 11:46:42 -0500

    Simplifying install

--- server_config.ps1
+++ server_config.ps1
@@ -23,31 +23,12 @@ Configuration BasicIIS
             ValueType = "Dword"
             DependsOn = "[WindowsFeature]IIS"
         }
-        Script NugetPackageProvider {
-            SetScript = {Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force}
-            TestScript = {if ((Get-PackageProvider -listavailable -name nuget -erroraction SilentlyContinue).Count -eq 0) {return $false}
-                else {return $true}}
-            GetScript = {@{Result = "true"}}       
-        }
-        Script ChocolateyPackageProvider {
-            SetScript = {Register-PackageSource -Name chocolatey -ProviderName Chocolatey -Location http://chocolatey.org/api/v2/ -force}
-            TestScript = {if ((Get-PackageProvider -listavailable -name Chocolatey -erroraction SilentlyContinue).Count -eq 0) {return $false}
-                else {return $true}}
-            GetScript = {@{Result = "true"}}       
-        }
-        Script xRemoteFile {
-            SetScript = {Install-Module           xPSDesiredStateConfiguration         }
-            TestScript = {if (get-dscresource -erroraction SilentlyContinue) {return $false}
-                else {return $true}}
-            GetScript = {@{Result = "true"}}
-            DependsOn = "[Script]NugetPackageProvider"
-        }
     }
 }
 
 BasicIIS -OutputPath .\BasicIIS
 Start-DscConfiguration -Wait -Verbose -Path .\BasicIIS -Force
 remove-item .\basicIIS -Force -Recurse
-Install-Package dotnetcore-windowshosting -ProviderName Chocolatey 
+
 
 
